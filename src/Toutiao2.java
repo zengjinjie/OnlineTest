027fb1c9d33496cb7947dc5a25004ded7e92b814
zengjinjie <zengjinjie@gmail.com>
2017-04-14 15:52:33 +0800

    test git

--- src/Toutiao2.java
+++ src/Toutiao2.java
@@ -5,4 +5,9 @@ import java.util.Scanner;
 /**
  * Created by zengjinjie on 2017/3/30 0030.
  */
+public class Toutiao2 {
+    public static void main(String[] args) {
+        System.out.printf("Test git!");
+    }
+}
 
